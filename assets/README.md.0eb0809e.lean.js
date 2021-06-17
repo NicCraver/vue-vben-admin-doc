c3df6ec1db96c964470ab9b615ce2131269f918d
anncwb <anncwb@users.noreply.github.com>
2021-06-17 15:38:50 +0000

    Automated deployment: Thu Jun 17 15:38:50 UTC 2021 21491bf7830a6d3b7f11dd735389f28f73334567

---
+++ assets/README.md.0eb0809e.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as a,a as s}from"./app.9632d56c.js";const e='{"title":"vben-admin-docs","description":"","frontmatter":{},"headers":[{"level":2,"title":"如何本地开发","slug":"如何本地开发"}],"relativePath":"README.md","lastUpdated":1623944308559}',t={},c=s('',3);t.render=function(s,e,t,o,d,i){return n(),a("div",null,[c])};export default t;export{e as __pageData};
